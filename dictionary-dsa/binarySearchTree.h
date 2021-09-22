fed525a83cb20d1a6635cf13a88677ee1a2d0188
HÙNG BÙI QUỐC <20127508@student.hcmus.edu.vn>
2021-09-22 16:15:44 +0700

    new folder

---
+++ dictionary-dsa/binarySearchTree.h
@@ -0,0 +1,20 @@
+#ifndef _BINARYSEARCHTREE_H_
+#define _BINARYSEARCHTREE_H_
+#include"utilities.h"
+
+void initialize(BinarySearchTree& bst); 
+nodeBST* newNodeBST(wstring keyword, wstring meaning);
+nodeBST* pushNodeBST(nodeBST* p, wstring keyword, wstring meaning);
+void readFileBST(BinarySearchTree &bst);
+void traverseBST(nodeBST* p, wfstream& fo);
+void writeFileBST(BinarySearchTree bst); 
+nodeBST* searchWordBST(nodeBST* p, wstring key);
+void findingWordBST(BinarySearchTree bst); 
+void insertingWordBST(BinarySearchTree bst); 
+void editingWordBST(BinarySearchTree bst);
+nodeBST* deleteNodeBST(nodeBST* p, wstring keyword);
+void deletingWordBST(BinarySearchTree bst);
+void destroyTree(nodeBST*& p); 
+
+
+#endif // !_BINARYSEARCHTREE_H_
---
+++ dictionary-dsa/utilities.h
@@ -0,0 +1,86 @@
+#ifndef _UTILITIES_H_
+#define _UTILITIES_H_
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<chrono>
+#include<vector>
+#include <cstdlib>
+#include <ctime>
+#define NUM_LINE 72320
+#define SOURCE_DATASET "Oxford-English-Dictionary.txt"
+#define RESULT_SET "EnglishDictionaryfromOxford.txt"
+#define INP_DESTINATION "input.txt"
+#define OUT_DESTINATION "output.txt"
+#define UTF8_ENCRYPTED_MASK "3NcRyPt3d"
+#define OUTLIER_HASHING_STRING "CoMputErsCiEnceiSmYlIf3333"
+using namespace std; 
+using chrono::steady_clock; 
+using chrono::duration; 
+struct hashElement {
+	wstring keyword;
+	wstring meaning;
+};
+struct Node {
+	hashElement element;
+	Node* next;
+};
+
+struct word {
+	string encrypted; 
+	string cmp; 
+	string in; 
+	string mean; 
+};
+
+struct headNode {
+	Node* head;
+};
+
+struct nodeBST {
+	wstring keyword; 
+	wstring meaning; 
+	nodeBST* left;
+	nodeBST* right;
+};
+struct BinarySearchTree {
+	nodeBST* root;
+};
+
+struct nodeAVL {
+	wstring keyword;
+	wstring meaning; 
+	int weight;
+	nodeAVL* lChild;
+	nodeAVL* rChild;
+};
+struct AVLTree {
+	nodeAVL* root;
+};
+
+struct nodeRB {
+	wstring keyword;
+	wstring meaning;
+	bool color; // red : 1, black : 0 
+	nodeRB* parent;
+	nodeRB* left;
+	nodeRB* right;
+};
+struct RedBlackTree {
+	nodeRB* root;
+};
+
+
+int max(int& a, int& b); 
+
+void swapHE(hashElement& a, hashElement& b); 
+
+string formalize(string w); 
+string encrypt(string root);
+
+wstring wformalize(wstring w);
+hashElement wfilter(wstring raw);
+
+int locatePrime(int size);
+#endif // !_UTILITIES_H_
+
